debbc5425ca487c58240b0e9ccba836d73f45271
Giovanni Celotto <giovanni.celottog@gmail.com>
2024-05-26 14:53:42 +0200

    update

--- scripts/tuplizer/job.sh
+++ scripts/tuplizer/job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G                       # 2G for Data needed   
-#SBATCH --partition=standard              # Specify your cluster partition
+#SBATCH --mem=12G                         
+#SBATCH --partition=short              # Specify your cluster partition
 #SBATCH --time=1:00:00  
 #SBATCH --output=/t3home/gcelotto/slurm/output/BTV.out  # Output file for stdout
 #SBATCH --error=/t3home/gcelotto/slurm/output/BTV.out    # Output file for stderr
 #SBATCH --dependency=singleton
 
-python /t3home/gcelotto/BTV/scripts/tuplizer/ntupleNew.py $1 $2  # to pass all the arguments
+python /t3home/gcelotto/BTV/scripts/tuplizer/ntupleLinker.py $1 $2  # to pass all the arguments
 source_dir="/scratch"
 fileName="$1"
 fileNumber="$2" 
