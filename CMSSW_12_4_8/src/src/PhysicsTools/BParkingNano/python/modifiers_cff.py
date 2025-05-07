d0a5b11439bfdbe2cd04b6cff9ca52b11a046ea1
Giovanni Celotto <giovanni.celottog@gmail.com>
2025-05-07 14:31:44 +0200

    Remove nested src folder and clean up

--- CMSSW_12_4_8/src/src/PhysicsTools/BParkingNano/python/modifiers_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-BToKMuMu_OpenConfig =  cms.Modifier()
-BToKEE_OpenConfig =  cms.Modifier()
-BToKEE_DiEle =  cms.Modifier()
-BToKMuMu_DiMuon =  cms.Modifier()
